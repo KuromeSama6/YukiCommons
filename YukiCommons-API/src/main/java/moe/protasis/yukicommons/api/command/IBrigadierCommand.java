541a059a3ad5ecb5fb4a6a80dec66ffd8543095d
KuromeSama6 <kuromesama6@gmail.com>
2026-03-05 16:39:18 -0500

    update

--- YukiCommons-API/src/main/java/moe/protasis/yukicommons/api/command/IBrigadierCommand.java
+++ YukiCommons-API/src/main/java/moe/protasis/yukicommons/api/command/IBrigadierCommand.java
@@ -4,7 +4,10 @@ import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
 import moe.protasis.yukicommons.api.exception.command.PermissionDeniedException;
+
+import java.util.Collection;
 
 public interface IBrigadierCommand {
     void Build(LiteralArgumentBuilder<IAbstractCommandExecutor> builder);
@@ -25,5 +28,23 @@ public interface IBrigadierCommand {
 
     default <T> RequiredArgumentBuilder<IAbstractCommandExecutor, T> RequireArgument(String name, ArgumentType<T> type) {
         return RequiredArgumentBuilder.argument(name, type);
+    }
+
+    default SuggestionProvider<IAbstractCommandExecutor> Suggest(Collection<String> suggestions) {
+        return Suggest(suggestions, false);
+    }
+
+    default SuggestionProvider<IAbstractCommandExecutor> Suggest(Collection<String> suggestions, boolean caseSensitive) {
+        return (ctx, b) -> {
+            suggestions.forEach(c -> {
+                if (caseSensitive) {
+                    if (c.startsWith(b.getRemaining())) b.suggest(c);
+
+                } else {
+                    if (c.toLowerCase().startsWith(b.getRemainingLowerCase())) b.suggest(c);
+                }
+            });
+            return b.buildFuture();
+        };
     }
 }
